3d2d865d18ef627b4898f98229b984b390722910
dev2 <dev2@mail.com>
2020-12-03 08:55:39 +0530

    change from eclipse

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello #from dev2
 {
 	public static void main()	#from dev2
 	{
-		system.out.println("hello from dev1");
+		system.out.println("hello from dev1..modify eclipse");
 	{
 }#from dev2
